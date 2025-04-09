6eba54c0260cf4bd24f36bf3db08ebef032bc5e1
Cccc YSU <you_iac@icloud.com>
2025-04-09 15:55:12 +0800

    添加readme文件

--- 3d_visable.py
+++ 3d_visable.py
@@ -1,11 +1,9 @@
 import numpy as np
 from mayavi import mlab
 from scipy.ndimage import binary_dilation, binary_erosion
+
 if __name__ == "__main__":
-    # s = np.load("D:/data/PCB10011008700.npy")
-    # s = np.load("D:\data/UNetPP_800_50/numpy/kerry3d.npy")
-    s = np.load("D:\PCB10011008700_R.npy")
-    # s = s + f*50
+    s = np.load("D:/0.npy")
     #绘制两个方向的切平面
     mlab.pipeline.image_plane_widget(mlab.pipeline.scalar_field(s), #scalar_field获得数据的标量数据场
                                      plane_orientation="x_axes",    #设置切平面的方向
@@ -21,5 +19,5 @@ if __name__ == "__main__":
                                      slice_index=100
                                      )
     #为这个数据绘制外框
-    # mlab.outline()
+    mlab.outline()
     mlab.show()
